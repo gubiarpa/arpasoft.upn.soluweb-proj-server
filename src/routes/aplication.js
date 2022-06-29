9718e72f35c77dd91f959a789e3a3d40be15d42d
Giordan Arredondo <garredondop@uni.pe>
2022-06-28 20:08:43 -0500

    routes

---
+++ src/routes/aplication.js
@@ -0,0 +1,12 @@
+const { Router } = require('express');
+const { 
+    aplicationGet,
+    aplicationPost,
+} = require('../controllers')
+
+const router = Router();
+
+router.get('/', aplicationGet);
+router.post('/', aplicationPost);
+
+module.exports = router;
