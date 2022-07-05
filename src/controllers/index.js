f021042698b112eb1ca2460ed364d8c5d097cfc6
Billy Arredondo <billy.arredondo@pucp.pe>
2022-07-04 19:56:43 -0500

    Remove 'Aplication' Endpoint

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,11 +1,9 @@
-const aplication = require('./aplication');
 const shared = require('./shared');
 const inicio = require('./inicio');
 const product = require('./product');
 const contacto = require('./contacto');
 
 module.exports = {
-    ...aplication,
     ...shared,
     ...inicio,
     ...product,
--- src/models/index.js
+++ src/models/index.js
@@ -1,11 +1,9 @@
-const Aplication = require('./aplication');
 const Product = require('./product');
 const Inicio = require('./inicio');
 const Contacto = require('./contacto');
 const Shared = require('./shared');
 
 module.exports = {
-    Aplication,
     Shared,
     Product,
     Inicio,
